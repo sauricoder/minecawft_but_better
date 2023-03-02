3b5887256f315d9170914bec34e22e55a61b0f45
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-03-02 09:58:43 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,8 @@ function broHubthing (hubSprite: Sprite) {
     hubSprite.z = 200
     hubSprite.setFlag(SpriteFlag.RelativeToCamera, true)
     hubSprite.setPosition(12, 9)
-    hubSprite.setImage(image.create(scene.screenWidth() - 30, 4))
+    hubSprite.setImage(image.create(meterWidth, 4))
     hubSprite.y = 115
-    hubSprite.image.fill(5)
 }
 function createTechnoblade () {
     mySprite = sprites.create(img`
@@ -31,7 +30,14 @@ function createTechnoblade () {
     controller.moveSprite(mySprite)
     scene.cameraFollowSprite(mySprite)
 }
+function drawHUBMeter (percent: number, hubSprite: Sprite, onColor: number, offColor: number) {
+    hubSprite.image.fill(offColor)
+    fillWidth = percent * meterWidth + 100
+    hubSprite.image.fillRect(0, 0, fillWidth, hubSprite.height, onColor)
+}
 let mySprite: Sprite = null
+let fillWidth = 0
+let meterWidth = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 createTechnoblade()
 let Hunger = sprites.create(img`
@@ -52,4 +58,25 @@ let Hunger = sprites.create(img`
     . . . . . . . . . . . c 1 b c . 
     . . . . . . . . . . . . c c . . 
     `, SpriteKind.HUD)
+meterWidth = scene.screenWidth() - 20
 broHubthing(Hunger)
+drawHUBMeter(100, Hunger, 7, 1)
+fillWidth = 100
+forever(function () {
+    for (let index = 0; index < 10; index++) {
+        pause(1000)
+    }
+    fillWidth += -25
+    if (fillWidth == 100) {
+        drawHUBMeter(100, Hunger, 7, 1)
+    }
+    if (fillWidth == 75) {
+        drawHUBMeter(100, Hunger, 5, 1)
+    }
+    if (fillWidth == 50) {
+        drawHUBMeter(100, Hunger, 3, 1)
+    }
+    if (fillWidth == 25) {
+        drawHUBMeter(100, Hunger, 1, 1)
+    }
+})
